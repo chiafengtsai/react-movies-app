34ea6e58d5f3af21875c54c5957ea895b14441c5
Chia <chia@punters.com.au>
2018-12-25 23:02:31 +1100

    feat: add listGroup

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import Movie from "./movie";
 import Pagination from "./pagination";
+import ListGroup from "./listGroup";
+
 import { getMovies } from "../services/fakeMovieService";
+import { getGenres } from "../services/fakeGenreService";
 import { paginate } from "../utils/paginate";
 
 class Movies extends Component {
   state = {
     movies: getMovies(),
     currentPage: 1,
-    pageSize: 4
+    pageSize: 4,
+    currentGenre: "All Genres",
+    genres: getGenres()
   };
 
   handleDelete = movie => {
@@ -20,14 +25,29 @@ class Movies extends Component {
     this.setState({ currentPage: page });
   };
 
+  handleGenre = genre => {
+    this.setState({ currentGenre: genre.name });
+  };
+
+  handleShowAllGenres = () => {
+    this.setState({ currentGenre: "All Genres" });
+  };
+
   render() {
     const { length: count } = this.state.movies;
     const { pageSize, currentPage, movies: allMovies } = this.state;
 
     if (count === 0) return <h1>There are no movies in the database.</h1>;
     const movies = paginate(allMovies, currentPage, pageSize);
+
     return (
       <React.Fragment>
+        <ListGroup
+          genres={this.state.genres}
+          showAllGenres={this.handleShowAllGenres}
+          currentGenre={this.state.currentGenre}
+          handleGenre={this.handleGenre}
+        />
         <p>Showing {count} movies in the database.</p>
         <table className="table">
           <thead>
--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import _ from "lodash";
 
 const Pagination = props => {
@@ -19,9 +20,17 @@ const Pagination = props => {
             </a>
           </li>
         ))}
+        `
       </ul>
     </nav>
   );
+};
+
+Pagination.propTypes = {
+  itemCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired
 };
 
 export default Pagination;
