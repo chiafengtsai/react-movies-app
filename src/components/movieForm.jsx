bbf952163a96df4e087b8dea8ac022e38f878a3c
Chia <chia@punters.com.au>
2019-01-05 18:26:20 +1100

    feat: add programmatic navigation.

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 
-const MovieForm = ({ match }) => {
-  return <h1>Movie Form {match.params.id}</h1>;
-};
+class MovieForm extends Component {
+  handleClick = () => {
+    this.props.history.push("/movies");
+  };
+  render() {
+    const { match } = this.props;
+    return (
+      <React.Fragment>
+        <h1>Movie Form {match.params.id}</h1>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={this.handleClick}
+        >
+          Save
+        </button>
+      </React.Fragment>
+    );
+  }
+}
 
 export default MovieForm;
